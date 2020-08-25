46b7ba3dd95ba6ff7c7d7c075bc1d08a57743b72
liushuhon <815835778@qq.com>
2020-08-25 16:19:15 +0800

    feat(element): 主题更改探索

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     <div class="home">
         <img alt="Vue logo"
              src="../assets/logo.png">
+             <el-button>aaa</el-button>
         <editor></editor>
     </div>
 </template>
